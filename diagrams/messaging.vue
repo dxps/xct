53828cd51462d9ce84f5f6978e961380894fd5aa
vision8 tech <vision8r@gmail.com>
2017-05-07 21:17:40 +0300

    messaging diag started

---
+++ diagrams/messaging.vue
@@ -0,0 +1,139 @@
+<!-- Tufts VUE 3.3.0 concept-map (messaging.vue) 2017-05-07 -->
+<!-- Tufts VUE: http://vue.tufts.edu/ -->
+<!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/files/kits/org/VUE/VUE-3.3.0.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
+<!-- Do Not Remove: Saved date Sun May 07 21:16:50 EEST 2017 by vision8 on platform Mac OS X 10.12.4 in JVM 1.8.0_121-b13 -->
+<!-- Do Not Remove: Saving version @(#)VUE: built October 8 2015 at 1658 by tomadm on Linux 2.6.32-504.23.4.el6.x86_64 i386 JVM 1.7.0_21-b11(bits=32) -->
+<?xml version="1.0" encoding="US-ASCII"?>
+<LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="none" ID="0" label="messaging.vue"
+    created="1494180429225" x="0.0" y="0.0" width="1.4E-45"
+    height="1.4E-45" strokeWidth="0.0" autoSized="false">
+    <resource referenceCreated="1494181010408"
+        spec="/Volumes/Macintosh HD/files/dev/vision8tech/xct/diagrams/messaging.vue"
+        type="1" xsi:type="URLResource">
+        <title>messaging.vue</title>
+        <property key="File" value="/Volumes/Macintosh HD/files/dev/vision8tech/xct/diagrams/messaging.vue"/>
+    </resource>
+    <fillColor>#FFFFFF</fillColor>
+    <strokeColor>#404040</strokeColor>
+    <textColor>#000000</textColor>
+    <font>SansSerif-plain-14</font>
+    <URIString>http://vue.tufts.edu/rdf/resource/e420dbea7f00000160b8d4868defdb9b</URIString>
+    <child ID="7" label="Messaging based comm." layerID="1"
+        created="1494180462296" x="270.0" y="40.0" width="204.0"
+        height="22.0" strokeWidth="0.0" autoSized="false" xsi:type="text">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Sys___Book-plain-16</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/e420dbea7f00000160b8d486469235f4</URIString>
+        <richText>&lt;html&gt;
+  &lt;head&gt;
+    &lt;style type="text/css"&gt;
+      &lt;!--
+        body { margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; font-size: 12; font-family: Arial; color: #000000 }
+        ol { margin-top: 6; font-family: Arial; vertical-align: middle; margin-left: 30; font-size: 12; list-style-position: outside }
+        p { margin-top: 0; margin-left: 0; margin-right: 0; margin-bottom: 0; color: #000000 }
+        ul { margin-top: 6; font-size: 12; margin-left: 30; vertical-align: middle; list-style-position: outside; font-family: Arial }
+      --&gt;
+    &lt;/style&gt;
+    
+  &lt;/head&gt;
+  &lt;body&gt;
+    &lt;p&gt;
+      &lt;font face="Sys___Book" style="font-size:16;"&gt;Messaging based comm&lt;/font&gt;.
+    &lt;/p&gt;
+  &lt;/body&gt;
+&lt;/html&gt;
+</richText>
+        <label>Messaging based comm.</label>
+    </child>
+    <child ID="9" label="message" layerID="1" created="1494180634507"
+        x="240.0" y="179.5" width="117.75" height="85.5"
+        strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <fillColor>#EAEAFF</fillColor>
+        <strokeColor>#A6A6A6</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Sys___Book-plain-15</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/e420dbeb7f00000160b8d486facdd7e8</URIString>
+        <child ID="10" label="header" created="1494180698207" x="34.0"
+            y="27.0" width="104.0" height="33.0" strokeWidth="1.0"
+            autoSized="false" xsi:type="node">
+            <fillColor>#FFFFFF</fillColor>
+            <strokeColor>#A6A6A6</strokeColor>
+            <textColor>#000000</textColor>
+            <font>Sys___Book-plain-18</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/e420dbec7f00000160b8d486d32d01ee</URIString>
+            <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+        </child>
+        <child ID="11" label="body" created="1494180704803" x="34.0"
+            y="54.75" width="104.0" height="33.0" strokeWidth="1.0"
+            autoSized="false" xsi:type="node">
+            <fillColor>#FFFFFF</fillColor>
+            <strokeColor>#A6A6A6</strokeColor>
+            <textColor>#000000</textColor>
+            <font>Sys___Book-plain-18</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/e420dbf37f00000160b8d48624352384</URIString>
+            <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+        </child>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <layer ID="1" label="Layer 1" created="1494180429231" x="0.0"
+        y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+        <URIString>http://vue.tufts.edu/rdf/resource/e420dbf37f00000160b8d486e1c57e46</URIString>
+    </layer>
+    <userZoom>1.0</userZoom>
+    <userOrigin x="-14.0" y="-14.0"/>
+    <presentationBackground>#202020</presentationBackground>
+    <PathwayList currentPathway="0" revealerIndex="-1">
+        <pathway ID="0" label="Untitled Pathway" created="1494180429224"
+            x="0.0" y="0.0" width="1.4E-45" height="1.4E-45"
+            strokeWidth="0.0" autoSized="false" currentIndex="-1" open="true">
+            <strokeColor>#B3993333</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/e420dbf47f00000160b8d486e9b94041</URIString>
+            <masterSlide ID="2" created="1494180429286" x="0.0" y="0.0"
+                width="800.0" height="600.0" locked="true"
+                strokeWidth="0.0" autoSized="false">
+                <fillColor>#000000</fillColor>
+                <strokeColor>#404040</strokeColor>
+                <textColor>#000000</textColor>
+                <font>SansSerif-plain-14</font>
+                <URIString>http://vue.tufts.edu/rdf/resource/e420dbf47f00000160b8d486ef865571</URIString>
+                <titleStyle ID="3" label="Header"
+                    created="1494180429367" x="341.0" y="175.0"
+                    width="118.0" height="50.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-36</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/e420dbf57f00000160b8d486b76622ca</URIString>
+                    <shape xsi:type="rectangle"/>
+                </titleStyle>
+                <textStyle ID="4" label="Slide Text"
+                    created="1494180429368" x="349.5" y="282.5"
+                    width="101.0" height="35.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-22</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/e420dbf67f00000160b8d486daf0d904</URIString>
+                    <shape xsi:type="rectangle"/>
+                </textStyle>
+                <linkStyle ID="5" label="Links" created="1494180429369"
+                    x="375.5" y="385.0" width="49.0" height="30.0"
+                    strokeWidth="0.0" autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#B3BFE3</textColor>
+                    <font>Gill Sans-plain-18</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/e420dbf77f00000160b8d486cb70c45a</URIString>
+                    <shape xsi:type="rectangle"/>
+                </linkStyle>
+            </masterSlide>
+        </pathway>
+    </PathwayList>
+    <date>2017-05-07</date>
+    <modelVersion>6</modelVersion>
+    <saveLocation>/Volumes/Macintosh HD/files/dev/vision8tech/xct/diagrams</saveLocation>
+    <saveFile>/Volumes/Macintosh HD/files/dev/vision8tech/xct/diagrams/messaging.vue</saveFile>
+</LW-MAP>
